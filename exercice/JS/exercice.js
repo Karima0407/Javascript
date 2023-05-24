f13a91177fcf15cd21c2582a280a18f43d4a79ba
KARIMA <karima.jamazi@myyahoo.com>
2023-05-24 15:18:40 +0200

    comit

---
+++ exercice/JS/exercice.js
@@ -0,0 +1,25 @@
+// var prenom = "karima";
+// var monNumero = 0616223;
+// console.log(prenom);
+// alert("Mitra");
+// window.alert("Super,tu es arrivé sur mon site !")
+// document.write("A la pause vous aurez des Haribow Pik!")
+// window.prompt("Question:on est quel jour?" ,"jour de la semaine");
+// var monPrenom=prompt();
+// document.write(monPrenom);
+// var unBooleen= false;
+// console.log(unBooleen);
+// unBooleen= "true";
+// console.log(unBooleen);
+// console.log(typeof unBooleen);
+// const PAYS ="France";
+// document.write(PAYS);
+// var unChiffre="12";
+// console.log(unChiffre)
+var monPrenom;
+monPrenom = prompt();
+document.write(monPrenom);
+console.log(monPrenom);
+alert(monPrenom);
+// Différentes types de fonctions:
+// 1-une fonction avec aumoins

ba081dd0421dccf4f3e7dc76ad6838058ffe008b
KARIMA <karima.jamazi@myyahoo.com>
2023-06-02 17:09:55 +0200

    comit6

--- exercice/JS/exercice.js
+++ exercice/JS/exercice.js
@@ -81,11 +81,17 @@
 //   j++; // j = j + 1; OU j += 1;
 // }
 // "<hr>"
-var monCompte = 1000;
-var temps = 0;
-while(monCompte < 2000) {
-    monCompte += 50;
-    temps++;
-}
-document.write("<h3>Sous sur mon compte : " + monCompte + "</h3>");
-document.write("<h3>Jusqu'à 2000€, il me faut : " + temps + " mois</h3>");
+// var monCompte = 1000;
+// var temps = 0;
+// while(monCompte < 2000) {
+//     monCompte += 50;
+//     temps++;
+// }
+// document.write("<h3>Sous sur mon compte : " + monCompte + "</h3>");
+// document.write("<h3>Jusqu'à 2000€, il me faut : " + temps + " mois</h3>");
+
+
+
+var fruits = ["Apple", "Banana"];
+fruits.push("Mango");
+console.log(fruits.push("Mango"));

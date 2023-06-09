703ffa27e19aa730e5327eb721b6e3406cf1d43d
KARIMA <karima.jamazi@myyahoo.com>
2023-06-09 11:02:52 +0200

    comit8

--- exercice/JS/exercice.js
+++ exercice/JS/exercice.js
@@ -123,89 +123,105 @@
 // var celcius = (farhen - 32) / 1.8;
 // document.write(celcius);
 
-Un programme en js :
+// Un programme en js :
 
-1- Implanter une fonction qui s'appelle "affichagePrenoms2" qui prend successivement deux paramètres de type tableau(remplis de 12 prénoms) et string 
-2- La fonction doit retourner un tableau qui contient deux éléments, le premier sera paramètre tableau, deuxième sera la chaîne de caractère
+// 1- Implanter une fonction qui s'appelle "affichagePrenoms2" qui prend successivement deux paramètres de type tableau(remplis de 12 prénoms) et string
+// 2- La fonction doit retourner un tableau qui contient deux éléments, le premier sera paramètre tableau, deuxième sera la chaîne de caractère
 
-3- nous allons afficher le résultat à partir de la console
+// 3- nous allons afficher le résultat à partir de la console
 
-var developpeurs = [
-  "Alexis",
+// var developpeurs = [
+//   "Alexis",
 
-  "Alin",
+//   "Alin",
 
-  "Cynthia",
+//   "Cynthia",
 
-  "Faïssal",
+//   "Faïssal",
 
-  "Jérémy",
+//   "Jérémy",
 
-  "Karima",
+//   "Karima",
 
-  "Michel",
+//   "Michel",
 
-  "Narcis",
+//   "Narcis",
 
-  "Nawal",
+//   "Nawal",
 
-  "Oliver",
+//   "Oliver",
 
-  "Rahim",
+//   "Rahim",
 
-  "Wassila",
-];
+//   "Wassila",
+// ];
 
-function affichagePrenoms2(paramArray, paramString) {
-  //Cela
-  var resultat = [paramArray, paramString];
-  // Nous avons déclaré la variable resultat (ici le type tableau) qui contient les paramètres utilisés par la fonction "affichagePrenoms2" et nous envoyons cette variable grâce au mot clé "return"
-  return resultat;
-  //Attention : on n'ajoute aucune syntaxe après la syntaxe "return", car elles ne seront pas executées(autrement dit elles ne seront pas prises en compte)
+// function affichagePrenoms2(paramArray, paramString) {
+//   //Cela
+//   var resultat = [paramArray, paramString];
+//   // Nous avons déclaré la variable resultat (ici le type tableau) qui contient les paramètres utilisés par la fonction "affichagePrenoms2" et nous envoyons cette variable grâce au mot clé "return"
+//   return resultat;
+//   //Attention : on n'ajoute aucune syntaxe après la syntaxe "return", car elles ne seront pas executées(autrement dit elles ne seront pas prises en compte)
+// }
+
+// // la syntaxe suivante fait deux opérations distinctes :
+// // 1 - Exécution de la fonction "affichagePrenoms2"
+// // 2- Stockage de la valeur de retour de notre fonction grâce au mot clé "return" utilisé à la fin de la fonction
+// // 3- Donc la variable "resultatDeLaFonction" a été créée afin de stocker la valeur de retour de notre fonction
+// var resultatDeLaFonction = affichagePrenoms2(
+//   developpeurs,
+//   "Mes développeurs sont géniaux"
+// );
+
+// var tabEnfant = ["Alexis", "Wassila"];
+// // Les deux syntaxes suivantes sont identiques, avec une différence significative: la première contient un tableau statique et la deuxième qui est bien appréciée contient un élément dynamique
+// //var tabFamille = [["Alexis", "Wassila"], "Mitra"];
+// var tabFamille = [tabEnfant, "Mitra"];
+
+// 1- Déclarer une fonction "additionDesChiffres" qui prend trois paramètres
+// 	- premier de type number "paramNum"
+//     - deuxième de type string "paramString"
+//     - troisème de type tableau "paramArray"
+
+// La fonction aura une valeur de retour
+
+// 2- Grâce à une boucle (le nombre de la condition dans la boucle vient du premier paramètre), la fonction "additionDesChiffres" va remplir le tableau "paramArray" avec les index de la boucle
+
+// 3- on affiche la valeur de retour sur la page web
+
+// function additionDesChiffres(paramNum, paramString, paramArray) {
+//   for (var i = 0; i <= paramNum; i++) {
+//     paramArray.push(i);
+//   }
+//   var result = [paramString, paramArray];
+//   return result;
+// }
+
+// // var valeurRetour = additionDesChiffres(15, "Le résultat de notre fonction : ", []);
+
+// var num = 15;
+// var chaineCaractere = "Le résultat de notre fonction : ";
+// var tab = [];
+// var valeurRetour = additionDesChiffres(num, chaineCaractere, tab);
+
+// document.write(valeurRetour);
+
+// // exercice permutation
+// var nombre1=5;
+// var nombre2=3;
+// var nombre3;
+// nombre3=nombre1;
+// nombre1=nombre2;
+// nombre2=nombre3;
+// // console.log('nombre1':+ );
+
+// console.log('nombre1 après inversion: '+ nombre1);
+// console.log('nombre2 après inversion:'+ nombre2);
+
+// exercice2:
+var num = prompt("Indiquez votre age : ");
+
+while (num <= 3 || num >= 120 || isNaN(num)) {
+  num = prompt("Indiquez votre age : ");
 }
-
-// la syntaxe suivante fait deux opérations distinctes :
-// 1 - Exécution de la fonction "affichagePrenoms2"
-// 2- Stockage de la valeur de retour de notre fonction grâce au mot clé "return" utilisé à la fin de la fonction
-// 3- Donc la variable "resultatDeLaFonction" a été créée afin de stocker la valeur de retour de notre fonction
-var resultatDeLaFonction = affichagePrenoms2(
-  developpeurs,
-  "Mes développeurs sont géniaux"
-);
-
-var tabEnfant = ["Alexis", "Wassila"];
-// Les deux syntaxes suivantes sont identiques, avec une différence significative: la première contient un tableau statique et la deuxième qui est bien appréciée contient un élément dynamique
-//var tabFamille = [["Alexis", "Wassila"], "Mitra"];
-var tabFamille = [tabEnfant, "Mitra"];
-
-
-
-1- Déclarer une fonction "additionDesChiffres" qui prend trois paramètres
-	- premier de type number "paramNum"
-    - deuxième de type string "paramString"
-    - troisème de type tableau "paramArray"
-    
-La fonction aura une valeur de retour
-    
-2- Grâce à une boucle (le nombre de la condition dans la boucle vient du premier paramètre), la fonction "additionDesChiffres" va remplir le tableau "paramArray" avec les index de la boucle 
-
-3- on affiche la valeur de retour sur la page web
-
-
-function additionDesChiffres(paramNum, paramString, paramArray) {
-  for (var i = 0; i <= paramNum; i++) {
-    paramArray.push(i);
-  }
-  var result = [paramString, paramArray];
-  return result;
-}
-
-// var valeurRetour = additionDesChiffres(15, "Le résultat de notre fonction : ", []);
-
-var num = 15;
-var chaineCaractere = "Le résultat de notre fonction : ";
-var tab = [];
-var valeurRetour = additionDesChiffres(num, chaineCaractere, tab);
-
-document.write(valeurRetour);
-
+var majoriteFR = 18;
